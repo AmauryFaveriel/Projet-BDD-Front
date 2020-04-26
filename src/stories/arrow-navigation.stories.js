2a823dd01ec0213bbd09f767ab606e82ab1087cc
Rémi DOREAU <remi.d45@gmail.com>
2020-04-26 18:51:36 +0200

    feat(chore): add features + stories

--- src/stories/arrow-navigation.stories.js
+++ src/stories/arrow-navigation.stories.js
@@ -1,13 +1,16 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
+import { MemoryRouter } from 'react-router';
 import ArrowNavigation from '../components/molecules/arrow-navigation';
 
 // eslint-disable-next-line import/prefer-default-export
 export const ArrowNavigationComponent = (props) => (
-  <ArrowNavigation
-    testid="arrow-navigation"
-    onClickLeft={props.onClickLeft}
-    onClickRight={props.onClickRight}
-  />
+  <MemoryRouter>
+    <ArrowNavigation
+      testid="arrow-navigation"
+      onClickLeft={props.onClickLeft}
+      onClickRight={props.onClickRight}
+    />
+  </MemoryRouter>
 );
