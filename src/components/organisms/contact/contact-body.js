54eed925074bc3a982171a02cc08e25de60756fb
Rémi DOREAU <remi.d45@gmail.com>
2020-04-26 18:51:06 +0200

    feat(organisms): add components

--- src/components/organisms/contact/contact-body.js
+++ src/components/organisms/contact/contact-body.js
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 
 const Body = styled.div`
   display: flex;
-  flex-direction: row;
-  width: 100%;
+  flex-direction: column;
+  width: 70%;
   height: 100%;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default Body;
